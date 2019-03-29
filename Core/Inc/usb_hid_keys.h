0258700d76840f8d6eaa12e379417f445280939d
Prokhor <orlov_proxor@mail.ru>
2019-03-29 13:33:00 +0600

    maked send media key pause

--- Core/Inc/usb_hid_keys.h
+++ Core/Inc/usb_hid_keys.h
@@ -284,4 +284,28 @@
 #define KEY_MEDIA_REFRESH 0xfa
 #define KEY_MEDIA_CALC 0xfb
 
+
+
+//https://notes.iopush.net/custom-usb-hid-device-descriptor-media-keyboard/
+// USB media codes
+#define USB_HID_SCAN_NEXT 0x01
+#define USB_HID_SCAN_PREV 0x02
+#define USB_HID_STOP      0x04
+#define USB_HID_EJECT     0x08
+#define USB_HID_PAUSE     0x10
+#define USB_HID_MUTE      0x20
+#define USB_HID_VOL_UP    0x40
+#define USB_HID_VOL_DEC   0x80
+
+// USB keyboard codes
+#define USB_HID_MODIFIER_LEFT_CTRL   0x01
+#define USB_HID_MODIFIER_LEFT_SHIFT  0x02
+#define USB_HID_MODIFIER_LEFT_ALT    0x04
+#define USB_HID_MODIFIER_LEFT_GUI    0x08 // (Win/Apple/Meta)
+#define USB_HID_MODIFIER_RIGHT_CTRL  0x10
+#define USB_HID_MODIFIER_RIGHT_SHIFT 0x20
+#define USB_HID_MODIFIER_RIGHT_ALT   0x40
+#define USB_HID_MODIFIER_RIGHT_GUI   0x80
+#define USB_HID_KEY_L     0x0F
+
 #endif // USB_HID_KEYS
